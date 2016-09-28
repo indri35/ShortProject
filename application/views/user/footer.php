3b5caa60a780004ae8d4fd43708cf15e58ff567d
indrindry <indriyani.cs49@gmail.com>
2016-09-28 17:01:46 +0700

    home user

--- application/views/user/footer.php
+++ application/views/user/footer.php
@@ -1,27 +1,20 @@
-<footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <p>Sekretariat PPID – Pemerintah Kota Surabaya<br/>Jl. Jimerto No. 25-27 Lantai 6 Surabaya<br/>
-                    Copyright &copy; Your Website 2016</p>
-                </div>
-            </div>
+<div class="art-footer">
+    <div class="art-footer-t"></div>
+    <div class="art-footer-l"></div>
+    <div class="art-footer-b"></div>
+    <div class="art-footer-r"></div>
+        <div class="art-footer-text">
+            <p>Sekretariat PPID – Pemerintah Kota Surabaya </p>
+            <p>Jl. Jimerto No. 25-27 Lantai 6 Surabaya</p>
+            <p class="art-page-footer" style="color:#FFCC00"><b>Copyright © 2013 All Rights Reserved, Best View 1024 x 800</b></p>
         </div>
-    </footer>
-
-    <!-- jQuery -->
-    <script src="<?php echo base_url() ?>assets/user/js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="<?php echo base_url() ?>assets/user/js/bootstrap.min.js"></script>
-
-    <!-- Script to Activate the Carousel -->
-    <script>
-    $('.carousel').carousel({
-        interval: 5000 //changes the speed
-    })
-    </script>
-
+        <div class="cleared"></div>
+    </div>
+</div>
+<div class="cleared"></div>
+</div>
+</div>
+<div class="cleared"></div> 
+</div>
 </body>
-
 </html>
--- application/views/user/home.php
+++ application/views/user/home.php
@@ -1,51 +1,75 @@
-    <div class="container">
-        <div class="row">
-            <div class="box">
-                <div class="col-lg-4">
-                    <hr>
-                        <h2 class="intro-text text-center"><strong>Perorangan</strong></h2>
-                    <hr>
-                    <img class="img-responsive img-border img-center " src="<?php echo base_url() ?>assets/user/img/perorangan.jpg" alt="" >
-                    <hr class="visible-xs">
-                    <p class="text-justify">Pemohon informasi yang adalah orang perseorangan Warga Negara Indonesia (WNI) dengan persyaratan melampirkan fotokopi Kartu Tanda Penduduk (KTP).</p>
-                    <a href="#" class="btn btn-default btn-lg">ENTRY</a>
-                </div>
-                <div class="col-lg-4">
-                    <hr>
-                        <h2 class="intro-text text-center"><strong>Kelompok Berbadan Hukum</strong></h2>
-                    <hr>
-                    <img class="img-responsive img-border img-center" src="<?php echo base_url() ?>assets/user/img/kelompokhukum.jpg" alt="" height=209px width=131px>
-                    <hr class="visible-xs">
-                    <p class="text-justify">Pemohon informasi yang adalah lembaga berbadan hukum Indonesia dengan persyaratan melampirkan akta pendirian yang telah terdaftar di Kementerian Hukum dan Hak Azasi Manusia / Pengadilan Negeri.</p>
-                    <a href="#" class="btn btn-default btn-lg">ENTRY</a>
-                </div>
-                <div class="col-lg-4">
-                    <hr>
-                        <h2 class="intro-text text-center"><strong>Kelompok Tidak Berbadan Hukum</strong></h2>
-                    <hr>
-                    <img class="img-responsive img-border img-center" src="<?php echo base_url() ?>assets/user/img/kelompoktidakhukum.jpg" alt="" >
-                    <hr class="visible-xs">
-                    <p class="text-justify">Pemohon informasi yang adalah kelompok Warga Negara Indonesia (WNI) dengan persyaratan melampirkan tandatangan dan Kartu Tanda Penduduk seluruh anggota kelompok.</p>
-                    <a href="#" class="btn btn-default btn-lg">ENTRY</a>
-                </div>
+
+
+<!--<nav class="art-nav">
+    <ul class="art-hmenu">
+        <li><a href="home.html" class="active">Home</a></li>
+        <li><a href="artikel.html">Artikel</a></li>
+        <li><a href="berkas-berkala.html">Berkas Berkala</a></li>
+        <li><a href="login.html">Login</a></li>
+        <li><a href="tentang.html">Tentang</a></li>
+        <li><a href="kontak-kami.html">Kontak Kami</a></li>
+    </ul> 
+</nav>--><div class="art-sheet clearfix">
+    <div class="art-layout-wrapper">
+        <div class="art-content-layout">
+            <div class="art-content-layout-row">
+                <div class="art-layout-cell art-content">
+                    <article class="art-post art-article">
+
+                        <div class="art-postcontent art-postcontent-0 clearfix"><div class="art-content-layout-wrapper layout-item-0">
+                                <div class="art-content-layout layout-item-1">
+                                    <div class="art-content-layout-row">
+                                        <div class="art-layout-cell layout-item-2" style="width: 33%" >
+                                            <p style="text-align: center;"><img width="208" height="208" alt="" src="http://ppid.surabaya.go.id//images/with-a-tablet_422_225.jpg"></p>
+                                            <div style="background-image: initial; background-attachment: initial; background-origin: initial; background-clip: initial; background-color: rgb(48, 120, 191); line-height: 40px; background-position: initial initial; background-repeat: initial initial;"><h4>PERORANGAN</h4></div>
+                                            <p style="text-align: justify;margin:10px">Pemohon informasi yang adalah orang perseorangan Warga Negara Indonesia (WNI) dengan persyaratan melampirkan fotokopi Kartu Tanda Penduduk (KTP).</p>
+                                            <p style="margin:10px"><a href="http://ppid.surabaya.go.id/index.php/perorangan" class="art-button">Entry</a></p>
+                                        </div><div class="art-layout-cell layout-item-2" style="width: 34%" >
+                                            <p style="text-align: center;"><img width="208" height="208" alt="" src="http://ppid.surabaya.go.id//images/perusahaan%20futures.gif" class=""></p>
+                                            <div style="background: #993D5C; line-height: 40px"><h4>KELOMPOK BERBADAN HUKUM</h4></div>
+                                            <p style="text-align: justify;margin:10px">Pemohon informasi yang adalah lembaga berbadan hukum Indonesia dengan persyaratan melampirkan akta pendirian yang telah terdaftar di Kementerian Hukum dan Hak Azasi Manusia / Pengadilan Negeri.</p>
+                                            <p style="margin:10px"><a href="http://ppid.surabaya.go.id/index.php/kelbadanhukum" class="art-button">Entry</a></p>
+                                        </div><div class="art-layout-cell layout-item-2" style="width: 33%" >
+                                            <p style="text-align: center;"><img width="208" height="208" alt="" src="http://ppid.surabaya.go.id//images/iusergroup.jpg" class=""></p>
+                                            <div style="background: #5C991F; line-height: 40px"><h4>KELOMPOK TIDAK BERBADAN HUKUM</h4></div>
+                                            <p style="text-align: justify;margin:10px">Pemohon informasi yang adalah kelompok Warga Negara Indonesia (WNI) dengan persyaratan melampirkan tandatangan dan Kartu Tanda Penduduk seluruh anggota kelompok.</p>
+                                            <p style="margin:10px"><a href="http://ppid.surabaya.go.id/index.php/keltidakbadanhukum" class="art-button">Entry</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </article></div>
             </div>
         </div>
-
-        <div class="row">
-            <div class="box">
-                <div class="col-lg-12">
-                    <hr>
-                    <h2 class="intro-text text-center">
-                        <strong>Links</strong>
-                    </h2>
-                    <hr>
-                    <div class="col-lg-6">
-                        <a href="#" class="btn btn-default btn-block" weight=100%>INFORMASI PUBLIK KOTA SURABAYA</a>
-                        <a href="#" class="btn btn-default btn-block">HUMAS KOTA SURABAYA</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
     </div>
-    <!-- /.container -->
+</div>
+
+
+<div class="mdinamis">
+    <div class="title">Links</div>
+    <div class="kiri">
+        <ul><!--<marquee behavior="scroll" direction="up" onmouseover="this.stop();" onmouseout="this.start();">-->
+            <!--<li><a href="">osdfd</a></li>
+            <li><a href="">osdfd</a></li>
+            <li><a href="">osdfd</a></li>
+            <li><a href="">osdfd</a></li>
+            <li><a href="">osdfd</a></li>
+            <li><a href="">osdfd</a></li>
+            <li><a href="">osdfd</a></li>
+            <li><a href="">osdfd</a></li>-->
+            <li><center><a href="http://www.surabaya.go.id/ver5" class="art-button" style="width:350px" target="blank">Informasi Publik Kota Surabaya</a></center></li><li><center><a href="http://humas.surabaya.go.id/" class="art-button" style="width:350px" target="blank">Humas Kota Surabaya</a></center></li>        <!--</marquee>--></ul>
+    </div>
+    <div class="kanan">
+        <ul><!--<marquee behavior="scroll" direction="up" onmouseover="this.stop();" onmouseout="this.start();">-->
+            <!--<li><a href=""><img src="gambar/ok.gif" width="625px" height="100px"></a></li>
+            <li><a href=""><img src="gambar/ok.gif" width="625px" height="100px"></a></li>
+            <li><a href=""><img src="gambar/ok.gif" width="625px" height="100px"></a></li>
+            <li><a href=""><img src="gambar/ok.gif" width="625px" height="100px"></a></li>-->
+                    <!--</marquee>--></ul>
+    </div>
+</div>
+
+
+
+
