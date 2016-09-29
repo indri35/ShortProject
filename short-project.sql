d535d86743eaef4806ff9e668fbfa5159aa2d5d1
hilmisalim <hilmisalim11@gmail.com>
2016-09-29 14:56:50 +0700

    new sql/crud template

--- short-project.sql
+++ short-project.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.6
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Sep 28, 2016 at 02:31 PM
--- Server version: 5.6.16
--- PHP Version: 5.5.9
+-- Host: 127.0.0.1:3307
+-- Generation Time: Sep 29, 2016 at 02:55 PM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.23
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `short-project`
@@ -26,16 +26,23 @@ SET time_zone = "+00:00";
 -- Table structure for table `t_dokumen`
 --
 
-CREATE TABLE IF NOT EXISTS `t_dokumen` (
-  `id` int(16) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `t_dokumen` (
+  `id` int(16) NOT NULL,
   `kode_berkas` varchar(128) NOT NULL,
   `upload_at` date NOT NULL,
   `nama_berkas` varchar(128) NOT NULL,
   `kategori` varchar(128) NOT NULL,
   `deskripsi` varchar(128) NOT NULL,
-  `skpd` varchar(128) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `kode_skpd` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `t_dokumen`
+--
+
+INSERT INTO `t_dokumen` (`id`, `kode_berkas`, `upload_at`, `nama_berkas`, `kategori`, `deskripsi`, `kode_skpd`) VALUES
+(1, 'berkas111', '2016-09-06', 'berkas skpd 111', 'buku', 'buku test', 'skpd001sb'),
+(2, 'berkas222', '2016-09-14', 'berkas skpd 222', 'buku', 'buku aja', 'skpd12jkt');
 
 -- --------------------------------------------------------
 
@@ -43,15 +50,23 @@ CREATE TABLE IF NOT EXISTS `t_dokumen` (
 -- Table structure for table `t_request`
 --
 
-CREATE TABLE IF NOT EXISTS `t_request` (
-  `id` int(16) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `t_request` (
+  `id` int(16) NOT NULL,
   `nik_pemohon` int(32) NOT NULL,
   `request_at` date NOT NULL,
   `kode_berkas` varchar(128) NOT NULL,
-  `response_at` date NOT NULL,
-  `skpd` varchar(128) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `response_at` date DEFAULT NULL,
+  `kode_skpd` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `t_request`
+--
+
+INSERT INTO `t_request` (`id`, `nik_pemohon`, `request_at`, `kode_berkas`, `response_at`, `kode_skpd`) VALUES
+(1, 2565956, '2016-09-28', 'berkas1', NULL, 'skpd12jkt'),
+(2, 5654557, '2016-09-29', 'berkas134', NULL, 'skpd02gg'),
+(3, 5611124, '2016-09-28', 'berkas453', NULL, 'skpd12jkt');
 
 -- --------------------------------------------------------
 
@@ -59,13 +74,20 @@ CREATE TABLE IF NOT EXISTS `t_request` (
 -- Table structure for table `t_skpd`
 --
 
-CREATE TABLE IF NOT EXISTS `t_skpd` (
-  `id` int(16) NOT NULL AUTO_INCREMENT,
-  `kode_skpd` int(16) NOT NULL,
+CREATE TABLE `t_skpd` (
+  `id` int(16) NOT NULL,
+  `kode_skpd` varchar(25) NOT NULL,
   `nama` varchar(128) NOT NULL,
-  `singkatan` varchar(128) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `singkatan` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `t_skpd`
+--
+
+INSERT INTO `t_skpd` (`id`, `kode_skpd`, `nama`, `singkatan`) VALUES
+(1, 'skpd12jkt', 'SKPD Jakarta Selatan', 'SKPD Jaksel'),
+(2, 'skpd002sby', 'SKPD Surabaya', 'SKPD sby');
 
 -- --------------------------------------------------------
 
@@ -73,8 +95,8 @@ CREATE TABLE IF NOT EXISTS `t_skpd` (
 -- Table structure for table `t_user`
 --
 
-CREATE TABLE IF NOT EXISTS `t_user` (
-  `id` int(16) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `t_user` (
+  `id` int(16) NOT NULL,
   `hak_akses` int(16) NOT NULL,
   `nik` int(25) NOT NULL,
   `nama` varchar(128) NOT NULL,
@@ -82,10 +104,61 @@ CREATE TABLE IF NOT EXISTS `t_user` (
   `no_tlpn` varchar(128) NOT NULL,
   `no_hp` varchar(128) NOT NULL,
   `email` varchar(128) NOT NULL,
-  `ktp` varchar(128) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `ktp` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `t_dokumen`
+--
+ALTER TABLE `t_dokumen`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `t_request`
+--
+ALTER TABLE `t_request`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `t_skpd`
+--
+ALTER TABLE `t_skpd`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `t_user`
+--
+ALTER TABLE `t_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `t_dokumen`
+--
+ALTER TABLE `t_dokumen`
+  MODIFY `id` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `t_request`
+--
+ALTER TABLE `t_request`
+  MODIFY `id` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `t_skpd`
+--
+ALTER TABLE `t_skpd`
+  MODIFY `id` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `t_user`
+--
+ALTER TABLE `t_user`
+  MODIFY `id` int(16) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
