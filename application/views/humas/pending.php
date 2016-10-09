c0e777e3b26d82c669894129c9d85954fbc5c8bc
hilmisalim <hilmisalim11@gmail.com>
2016-10-09 18:43:56 +0700

    akhir sembilan oktober

--- application/views/humas/pending.php
+++ application/views/humas/pending.php
@@ -43,7 +43,7 @@
                       <td><?php echo $pen->nama_berkas;?></td>
                       <td><?php echo $pen->nik_pemohon;?></td>
                       <td><?php echo $pen->kode_skpd;?></td>
-                      <td><?php echo ($pen->berkas_upload==TRUE)? $req->date_upload : '<button type="button" class="btn btn-success">Tanggapi</button>'; ?>
+                      <td><?php echo ($pen->berkas_upload==TRUE)? $pen->berkas_upload : "<button type='button' class='btn btn-default'>".anchor('humas/tanggapi/'.$pen->no_req,'Tanggapi')."</button>"; ?>
                       </td>
                       <td><?php echo ($pen->berkas_upload==TRUE)? '<i class="fa fa-fw fa-check"></i>' : '<i class="fa fa-fw fa-close"></i>'; ?></td>
                     <?php $no++; };?>
